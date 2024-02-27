31387adf31e63d61f43f30817753b72589e30e9c
George Uzoh <ugochukwuuzoh@gmail.com>
2024-02-27 14:01:09 +0200

    #12: add tags to mushroom data

--- src/Mushroom.tsx
+++ src/Mushroom.tsx
@@ -1,30 +1,32 @@
-import React from 'react'
-import { useParams, useNavigate } from 'react-router-dom'
-import data from './data/data'
-
+import React from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import data from "./data/data";
 
 const Mushroom = () => {
-    const params = useParams()
-    const mushroomData = data.find((mushroom) => mushroom.name === params.mushroom)
-    const navigate = useNavigate()
+  const params = useParams();
+  const mushroomData = data.find(
+    (mushroom) => mushroom.name === params.mushroom
+  );
+  const navigate = useNavigate();
 
-    return (
-        <>
-    
-        <div>
-            <h1>{mushroomData?.name} <span className='italic'>({mushroomData?.scientificName})</span></h1>
-            <img src={mushroomData?.image} alt={mushroomData?.name} /> 
-            <p>{mushroomData?.description}</p>
-            <p>{mushroomData?.occurrence}</p>
-            <p>{mushroomData?.['picking months']}</p>
-            <p>{mushroomData?.['primary habitat']}</p>
-            <p>{mushroomData?.edible}</p>
-            <p>{mushroomData?.recipes}</p>
-            <button onClick={() => navigate(-1)}>Return</button>
-        </div>
-        
-        </>
-)
-}
+  return (
+    <>
+      <div>
+        <h1>
+          {mushroomData?.name}{" "}
+          <span className="italic">({mushroomData?.scientificName})</span>
+        </h1>
+        <img src={mushroomData?.image} alt={mushroomData?.name} />
+        <p>Description: {mushroomData?.description}</p>
+        <p>Occurence: {mushroomData?.occurrence}</p>
+        <p>Picking months: {mushroomData?.["picking months"]}</p>
+        <p>Primary habitat: {mushroomData?.["primary habitat"]}</p>
+        <p>Edible: {mushroomData?.edible}</p>
+        <p>Recipes: {mushroomData?.recipes}</p>
+        <button onClick={() => navigate(-1)}>Return</button>
+      </div>
+    </>
+  );
+};
 
-export default Mushroom
+export default Mushroom;
