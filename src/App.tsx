3e0b00a02b02fbd7c87fc3d585f547080ff6f8cb
George Uzoh <ugochukwuuzoh@gmail.com>
2024-02-28 09:11:23 +0200

    add About page and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,39 @@
-import type { FC } from 'react';
+import type { FC } from "react";
 import ModelProvider from "./ModelProvider";
 import Layout from "./components/Layout";
 
 import React from "react";
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Home from './Home';
-import Mushroom from './Mushroom';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./Home";
+import Mushroom from "./Mushroom";
+import About from "./pages/About";
 
-
-
-const App: FC =( ) => {
-const router = createBrowserRouter([
-{
-element: <Layout/>,
-children: [
+const App: FC = () => {
+  const router = createBrowserRouter([
     {
-        path:"/",
-        element: <Home/>
-
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: ":mushroom",
+          element: <Mushroom />,
+        },
+        {
+          path: "about",
+          element: <About />,
+        },
+      ],
     },
-    {
-      path:":mushroom",
-      element: <Mushroom/>
+  ]);
 
-  },
-
-]
-}
-
-])
-    
   return (
     <ModelProvider>
-    <RouterProvider router={router}/>
+      <RouterProvider router={router} />
     </ModelProvider>
   );
-}
+};
 
 export default App;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,13 @@
 import * as React from "react";
 import Image from "react-bootstrap/Image";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
     <header className="sticky top-0 z-50 bg-primary shadow-xl border-b-[1px] border-[#4b5563]">
       <div className="layout flex h-14 lg:h-16 items-center justify-between max-w-[90vw] lg:max-w-[80vw] 2xl:max-w-[1400px] mx-auto">
-        
-          <div className="flex justify-center items-center text-white text-xl mx-2">
+        <Link to="/" className="text-white text-xl mx-2">
+          <div className="flex justify-center items-center text-white">
             <Image
               alt=""
               src="https://i.ibb.co/qFZpvXs/logo.png"
@@ -14,8 +15,12 @@ export default function Header() {
               height={40}
             />
             <span className="mx-2">Mushrooms Checker</span>
-            </div>
-           </div>
+          </div>
+        </Link>
+        <Link to="/about" className="text-white text-xl">
+          About
+        </Link>
+      </div>
     </header>
   );
 }
