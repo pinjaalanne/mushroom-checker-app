8d48e8fde5d8eba260ffbd7b85fc4c365b3bde5b
George Uzoh <ugochukwuuzoh@gmail.com>
2024-02-28 09:15:56 +0200

    install mui

--- src/Mushroom.tsx
+++ src/Mushroom.tsx
@@ -1,71 +1,136 @@
 import React from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import data from "./data/data";
-import { Card, Box, CardContent, Typography, CardMedia, Button } from "@mui/material";
+import {
+  Card,
+  Box,
+  CardContent,
+  Typography,
+  CardMedia,
+  Button,
+} from "@mui/material";
 
 const Mushroom = () => {
-    const params = useParams();
-    const mushroomData = data.find(
-        (mushroom) => mushroom.name === params.mushroom
-    );
-    const navigate = useNavigate();
+  const params = useParams();
+  const mushroomData = data.find(
+    (mushroom) => mushroom.name === params.mushroom
+  );
+  const navigate = useNavigate();
 
-    return (
-        <>
-            <Card sx={{ display: 'flex' }}>
-                <CardMedia
-                    component="img"
-                    sx={{ maxHeight: '500px', marginLeft: '0', objectFit: "contain" }}
-                    image={mushroomData?.image}
-                    alt="Mushroom image"
-                />
-                <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-                    <CardContent sx={{ flex: '1 0 auto' }}>
-                        <Typography component="div" variant="h5">
-                            {mushroomData?.name}{" "}
-                            <span className="italic">({mushroomData?.scientificName})</span>
-                        </Typography>
-                        <Typography variant="subtitle1" color="text.secondary" component="div">
-                            Description:
-                        </Typography>
-                        <Typography variant="subtitle1" color="text.secondary" component="div">
-                            {mushroomData?.description}
-                        </Typography>
-                        <Typography variant="subtitle1" color="text.secondary" component="div">
-                            Occurence:
-                        </Typography>
-                        <Typography variant="subtitle1" color="text.secondary" component="div">
-                            {mushroomData?.occurrence}
-                        </Typography>
-                        <Typography variant="subtitle1" color="text.secondary" component="div">
-                            Picking months:
-                        </Typography>
-                        <Typography variant="subtitle1" color="text.secondary" component="div">
-                            {mushroomData?.["picking months"]}
-                        </Typography>
-                        <Typography variant="subtitle1" color="text.secondary" component="div">
-                            Primary habitat:
-                        </Typography>
-                        <Typography variant="subtitle1" color="text.secondary" component="div">
-                            {mushroomData?.["primary habitat"]}
-                        </Typography>
-                        <Typography variant="subtitle1" color="text.secondary" component="div">
-                            Edible:
-                        </Typography>
-                        <Typography variant="subtitle1" color="text.secondary" component="div">
-                            {mushroomData?.edible ? "True" : "False"}
-                        </Typography>
-                        <Typography variant="subtitle1" color="text.secondary" component="div">
-                            Recipes:
-                        </Typography>
-                        <Typography variant="subtitle1" color="text.secondary" component="div">
-                            {mushroomData?.recipes}
-                        </Typography>
-                        <Button sx={{ backgroundColor: '#d7c8c0', color: '#48362c', ":hover": { backgroundColor: '#a28778' } }} onClick={() => navigate(-1)}>Return</Button>
-                    </CardContent>
-                </Box>
-            </Card >
-        </>);
+  return (
+    <>
+      <Card sx={{ display: "flex" }}>
+        <CardMedia
+          component="img"
+          sx={{ maxHeight: "500px", marginLeft: "0", objectFit: "contain" }}
+          image={mushroomData?.image}
+          alt="Mushroom image"
+        />
+        <Box sx={{ display: "flex", flexDirection: "column" }}>
+          <CardContent sx={{ flex: "1 0 auto" }}>
+            <Typography component="div" variant="h5">
+              {mushroomData?.name}{" "}
+              <span className="italic">({mushroomData?.scientificName})</span>
+            </Typography>
+            <Typography
+              variant="subtitle1"
+              color="text.secondary"
+              component="div"
+            >
+              Description:
+            </Typography>
+            <Typography
+              variant="subtitle1"
+              color="text.secondary"
+              component="div"
+            >
+              {mushroomData?.description}
+            </Typography>
+            <Typography
+              variant="subtitle1"
+              color="text.secondary"
+              component="div"
+            >
+              Occurence:
+            </Typography>
+            <Typography
+              variant="subtitle1"
+              color="text.secondary"
+              component="div"
+            >
+              {mushroomData?.occurrence}
+            </Typography>
+            <Typography
+              variant="subtitle1"
+              color="text.secondary"
+              component="div"
+            >
+              Picking months:
+            </Typography>
+            <Typography
+              variant="subtitle1"
+              color="text.secondary"
+              component="div"
+            >
+              {mushroomData?.["picking months"]}
+            </Typography>
+            <Typography
+              variant="subtitle1"
+              color="text.secondary"
+              component="div"
+            >
+              Primary habitat:
+            </Typography>
+            <Typography
+              variant="subtitle1"
+              color="text.secondary"
+              component="div"
+            >
+              {mushroomData?.["primary habitat"]}
+            </Typography>
+            <Typography
+              variant="subtitle1"
+              color="text.secondary"
+              component="div"
+            >
+              Edible:
+            </Typography>
+            <Typography
+              variant="subtitle1"
+              color="text.secondary"
+              component="div"
+            >
+              {mushroomData?.edible ? "True" : "False"}
+            </Typography>
+            <Typography
+              variant="subtitle1"
+              color="text.secondary"
+              component="div"
+            >
+              Recipes:
+            </Typography>
+            <Typography
+              variant="subtitle1"
+              color="text.secondary"
+              component="div"
+            >
+              {mushroomData?.recipes}
+            </Typography>
+            <Button
+              sx={{
+                backgroundColor: "#d7c8c0",
+                color: "#48362c",
+                ":hover": { backgroundColor: "#a28778" },
+              }}
+              onClick={() => navigate(-1)}
+            >
+              Return
+            </Button>
+          </CardContent>
+        </Box>
+      </Card>
+    </>
+  );
 };
 
 export default Mushroom;
