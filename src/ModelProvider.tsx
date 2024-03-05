8dd5b44224984c168a755c76039af708af0ec51f
mtjomy17@gmail.com <mtjomy17@gmail.com>
2024-03-05 10:04:54 +0200

    comments in the code

--- src/ModelProvider.tsx
+++ src/ModelProvider.tsx
@@ -19,14 +19,21 @@ const ModelContext = createContext<ModelContextInterface>({
   model: null,
 });
 
+// Custom hook to consume the model from the context
 export const useModel = () => useContext(ModelContext);
 
+// Provider component responsible for loading and providing the machine learning model
 const ModelProvider = ({ children }: Props) => {
+
+  // State to store the loaded machine learning model
   const [model, setModel] = useState<tmImage.CustomMobileNet | null>(null);
 
+  // Effect to load the machine learning model on component mount
   useEffect(() => {
     const loadModel = async () => {
+      // Check if the model is not already loaded
       if (!model) {
+      // Load the model using Teachable Machine's load function
         const loadedModel = await tmImage.load(
           config.modelURL,
           config.metadataURL
