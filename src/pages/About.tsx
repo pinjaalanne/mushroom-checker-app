b704d104ed6b37b0c7374a555d6c87477813d5e5
mtjomy17@gmail.com <mtjomy17@gmail.com>
2024-03-08 13:34:17 +0200

    Ticket #19 about text

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -2,9 +2,34 @@ import React from "react";
 
 const About: React.FC = () => {
   return (
-    <div>
-      <h1>About Page</h1>
-    </div>
+ <main className="about-main">
+      <h1 className="about-title">About</h1>
+      <h2>Mushroom Checker App</h2>
+      <h3>Description</h3>
+      <p>
+        This project was created as an assignment typeScript class at <a href="https://www.bc.fi">Business College Helsinki</a>, REACT23S Full Stack Web Developer program. 
+      </p>
+      <p>This application is a web-based tool that uses a machine learning model to classify mushrooms images uploaded by the user.</p>
+      <h3>Contributors Team 4:</h3>
+      <ul className="about-list">
+      <li>Pinja Alanne</li>
+      <li>George</li>
+      <li>Mychel Garzon</li>  
+      </ul>
+      <h3>Tools</h3>
+      <ul className="about-list">
+      <li>Version control: Git and GitHub</li>
+      <li>Project management: Trello</li>
+      <li>Communication: Teams</li>
+      <li>Machine learning technology: Teachable Machine</li>
+      <li>Front-end library: React with TypeScript (ReactTs is not a standard term)</li>
+      <li>Language: TypeScript</li>
+      <li>Monitor of changes: Nodemon</li>
+      <li>Styling: @mui/material, Tailwind</li>
+      <li>CI/CD: SonarCloud - GitHub Actions</li>
+
+        </ul>
+    </main>
   );
 };
 
